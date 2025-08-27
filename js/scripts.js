caecf36b6966823eaa2bf6b1841c8f1aedb9affd
ShahriarTbc <ripon23105101019@diu.edu.bd>
2025-08-27 20:25:33 +0600

    Complete JS file work

---
+++ js/scripts.js
@@ -0,0 +1,86 @@
+const callHistoryWrapper = document.getElementById("call-history-wrapper");
+const heartCount = document.getElementById("heart-count");
+const copyCount = document.getElementById("copy-count");
+const callBtns = document.querySelectorAll(".call-btn");
+const historyClearBtn = document.getElementById("history-clear-btn");
+const heartBtns = document.querySelectorAll(".heart-btn");
+const copyBtns = document.querySelectorAll(".copy-btn");
+
+//Todo-------Manage Incoming Calls & Track Diamond Count-------
+function callFeature() {
+  let coinCount = document.getElementById("coin-count");
+  for (const callBtn of callBtns) {
+    callBtn.addEventListener("click", function () {
+      const helplineService =
+        callBtn.parentNode.parentNode.querySelector(
+          ".helpline-service"
+        ).innerText;
+      const helplineNumber =
+        callBtn.parentNode.parentNode.querySelector(
+          ".helpline-number"
+        ).innerText;
+
+      if (coinCount.innerText >= 20) {
+        coinCount.innerText -= 20;
+        alert(`Calling ${helplineService} ${helplineNumber}...`);
+        callHistoryWrapper.innerHTML += `
+                    <div class="single-call-history flex gap-4 justify-between items-center bg-[#fafafa] rounded-lg p-4 mb-2">
+                        <div>
+                            <h4 class="inter-semibold text-lg text-[#111111]">${helplineService}</h4>
+                            <p class="hind-madurai-regular text-lg text-[#5c5c5c]">${helplineNumber}</p>
+                        </div>
+                        <p class="hind-madurai-regular text-lg text-[#111111]" id="time">${new Date().toLocaleTimeString()}</p>
+                    </div>
+                `;
+      } else {
+        alert(
+          "⚠ Not enough coins to make a call. You need at least 20 coins."
+        );
+      }
+    });
+  }
+}
+
+//Todo------Track Bookmark Count------
+function getHeartCount() {
+  for (const heartBtn of heartBtns) {
+    heartBtn.addEventListener("click", function () {
+      heartCount.innerText++;
+    });
+  }
+}
+
+//Todo------Track Copy Button Clicks & Copy Helpline Number to Clipboard------
+function getCopiedCount() {
+  for (const copyBtn of copyBtns) {
+    copyBtn.addEventListener("click", function () {
+      alert("Hotline number copied to clipboard.");
+      //Todo------Increment Copy Button Counter by 1------
+      copyCount.innerText++;
+
+      //Todo------Copy Helpline Number to Clipboard------
+      const helplineNumber =
+        copyBtn.parentNode.parentNode.querySelector(
+          ".helpline-number"
+        ).innerText;
+      copyHotlineToClipboard(helplineNumber);
+    });
+  }
+}
+
+//Todo------Manage Clipboard Copy------
+function copyHotlineToClipboard(text) {
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    navigator.clipboard.writeText(text);
+  }
+}
+
+//Todo------Reset Call History Content------
+historyClearBtn.addEventListener("click", function () {
+  callHistoryWrapper.innerHTML = "";
+});
+
+//Todo------Execute Function------
+callFeature();
+getHeartCount();
+getCopiedCount();
